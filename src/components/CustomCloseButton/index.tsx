ca1008f0eaf5a318b913b8fc81e267f26bbb194a
Dorian <dorianpernot@mac-dorian.home>
2023-12-17 03:14:44 +0100

    first

---
+++ src/components/CustomCloseButton/index.tsx
@@ -0,0 +1,9 @@
+export function CustomCloseButton() {
+  return (
+      <img
+        src="https://media.giphy.com/media/l396M3jF14DXr9mog/giphy.gif"
+        alt="Funny Close GIF"
+        style={{ width: "50px", height: "auto", borderRadius: "5px" }}
+      />
+  );
+}
