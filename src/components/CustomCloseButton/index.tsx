e37257fb828b01bcf8fa82591fbbe005bde09f5a
Dorian <dorianpernot@mac-dorian.home>
2023-12-17 22:17:44 +0100

    test for deployment 2

--- src/components/CustomCloseButton/index.tsx
+++ src/components/CustomCloseButton/index.tsx
@@ -1,9 +1,8 @@
 export function CustomCloseButton() {
   return (
       <img
-        src="https://media.giphy.com/media/l396M3jF14DXr9mog/giphy.gif"
-        alt="Funny Close GIF"
-        style={{ width: "50px", height: "auto", borderRadius: "5px" }}
+        src="/src/assets/bye.jpeg"
+        alt="bye img"
       />
   );
 }
